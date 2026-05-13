ab8e535e9333d6ba342194544c519ba8948c10ac
Jens Timmerman <281523+JensTimmerman@users.noreply.github.com>
2026-05-13 20:31:28 +0200

    Renamed home water to hot water

--- guntamatic/heater.py
+++ guntamatic/heater.py
@@ -31,9 +31,6 @@ SENSORS = [
     "Program",
     SERIAL,
     "Version",
-]
-
-DIAGNOSTIC_SENSORS = [
     "Boil.shunt pump",
     "Suction fun",
     "Primary air",
@@ -77,6 +74,10 @@ DIAGNOSTIC_SENSORS = [
     "Program HC6",
     "Program HC7",
     "Program HC8",
+
+]
+
+DIAGNOSTIC_SENSORS = [
     "Interuption 1",
     "Operat. time",
     "Service Hrs",
@@ -105,10 +106,18 @@ TRANSLATE = {
     "Buffer Top": "buffer_top_temperature",
     "Buffer Mid": "buffer_center_temperature",
     "Buffer Btm": "buffer_bottom_temperature",
-    "DHW 0": "domestic_home_water_temperature",
+    "DHW 0": "domestic_hot_water_0_temperature",
+    "DHW 1": "domestic_hot_water_1_temperature",
+    "DHW 2": "domestic_hot_water_2_temperature",
     "Room Temp:HC 0": "room_0_temperature",
     "Room Temp:HC 1": "room_1_temperature",
     "Room Temp:HC 2": "room_2_temperature",
+    "Room Temp:HC 3": "room_3_temperature",
+    "Room Temp:HC 4": "room_4_temperature",
+    "Room Temp:HC 5": "room_5_temperature",
+    "Room Temp:HC 6": "room_6_temperature",
+    "Room Temp:HC 7": "room_7_temperature",
+    "Room Temp:HC 8": "room_8_temperature",
     "Program": "program",
     SERIAL: "serial",
     "Version": "version",
@@ -119,8 +128,64 @@ TRANSLATE = {
     "HIBERNAT": "hibernate",
     "HIBERNATE TO": "hibernate_to",
     "DHW BOOST": "dhw_boost",
+    "Boil.shunt pump": 'boiler_shunt_pump',
+    "Suction fun": 'suction_fan',
+    "Primary air": 'primary_air',
+    "Seconday air": 'secondary_air',
+    "CO2 Content": 'co2_content',
+    "DHW Pump 0": 'dhw_pump_0',
+    "DHW Pump 1": 'dhw_pump_1',
+    "DHW Pump 2": 'dhw_pump_2',
+    "Heating circulation pump 0": 'heating_circulation_pump_0',
+    "Heating circulation pump 1": 'heating_circulation_pump_1',
+    "Heating circulation pump 2": 'heating_circulation_pump_2',
+    "Heating circulation pump 3": 'heating_circulation_pump_3',
+    "Heating circulation pump 4": 'heating_circulation_pump_4',
+    "Heating circulation pump 5": 'heating_circulation_pump_5',
+    "Heating circulation pump 6": 'heating_circulation_pump_6',
+    "Heating circulation pump 7": 'heating_circulation_pump_7',
+    "Heating circulation pump 8": 'heating_circulation_pump_8',
+    "Flow is 0": 'circuit_0_temp',
+    "Flow is 1": 'circuit_1_temp',
+    "Flow is 2": 'circuit_2_temp',
+    "Flow is 3": 'circuit_3_temp',
+    "Flow is 4": 'circuit_4_temp',
+    "Flow is 5": 'circuit_5_temp',
+    "Flow is 6": 'circuit_6_temp',
+    "Flow is 7": 'circuit_7_temp',
+    "Flow is 8": 'circuit_8_temp',
+    "Program HC0": 'heating_circulation_program_0',
+    "Program HC1": 'heating_circulation_program_1',
+    "Program HC2": 'heating_circulation_program_2',
+    "Program HC3": 'heating_circulation_program_3',
+    "Program HC4": 'heating_circulation_program_4',
+    "Program HC5": 'heating_circulation_program_5',
+    "Program HC6": 'heating_circulation_program_6',
+    "Program HC7": 'heating_circulation_program_7',
+    "Program HC8": 'heating_circulation_program_8',
+}
+"""
+    "Interuption 1",
+    "Operat. time",
+    "Service Hrs",
+    "extra-WW. 1",
+    "extra-WW. 2",
+    "B extra-WW. 0",
+    "B extra-WW. 1",
+    "B extra-WW. 2",
+    "Buffer Top 0",
+    "Buffer Btm 0",
+    "Buffer Top 1",
+    "Buffer Btm 1",
+    "Buffer Top 2",
+    "Buffer Btm 2",
+    "Auxiliary pump 0",
+    "Auxiliary pump 1",
+    "Auxiliary pump 2",
+
 }
 
+"""
 
 class UnexpectedDataEncounteredException(Exception):
     """
@@ -187,6 +252,21 @@ class Heater():
                 out[TRANSLATE[key]] = data.get(key, None)
             except KeyError as exc:
                 raise UnexpectedDataEncounteredException from exc
+
+        # skip room temperature if it is 60: default value
+        for key in list(out):
+            if 'room' in key and (out[key] ==  ["60.00", "\u00b0C"] or out[key] ==  ["-9.00", "\u00b0C"]):
+                del out[key]
+            if 'domestic_hot_water' in key and out[key] ==  ["-20.00", "\u00b0C"]:
+                del out[key]
+            if 'circuit' in key and (
+                out[key] ==  ["43.00", "\u00b0C"] or out[key] == ["-20.00", "\u00b0C"]) :
+                circuit_nr = key[8]
+                del out[key]
+                del out[f'heating_circulation_pump_{circuit_nr}']
+                del out[f'heating_circulation_program_{circuit_nr}']
+            if key in out:
+             print(key, out[key])
         if 'serial' not in out or not out['serial']:
             raise NoSerialException
         # Translate values as wel for known enums
