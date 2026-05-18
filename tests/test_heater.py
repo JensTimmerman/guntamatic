9eacebe93b6096c4346818d7e239be2f9a334101
Jens Timmerman <281523+JensTimmerman@users.noreply.github.com>
2026-05-19 00:04:16 +0200

    Fixed tests

--- tests/test_heater.py
+++ tests/test_heater.py
@@ -26,7 +26,7 @@ def mock_serial_get(url: str, **kwargs) -> MagicMock:
     """Return mock response based on URL."""
     mock = MagicMock()
     if "daqdesc" in url:
-        mock.text = "Serial;\nOutside Temp.;°C\nreserved;°C\nProgram;\n;\n"
+        mock.text = "Serial;\nVersion;\nreserved;°C\nProgram;\n;\n"
     else:
         mock.text = MOCK_DATA
     return mock
@@ -48,63 +48,9 @@ def test_get_data(mock_requests, heater: Heater) -> None:
     data = heater.parse_data()
     print(data)
     assert data == {
-        'status': None,
-        'boiler_temperature': None,
-        'outdoor_temperature': ['15.95', '°C'],
-        'buffer_load': None,
-        'buffer_top_temperature': None,
-        'buffer_center_temperature': None,
-        'buffer_bottom_temperature': None,
-        'room_0_temperature': None,
-        'room_1_temperature': None,
-        'room_2_temperature': None,
+        'serial': ['14.09', ''],
+        'version': ['15.95', ''],
         'program': ['heat', ''],
-        'serial':  ['14.09', ''],
-        'version': None,
-        'boiler_shunt_pump': None,
-        'circuit_0_temp': None,
-        'circuit_1_temp': None,
-        'circuit_2_temp': None,
-        'circuit_3_temp': None,
-        'circuit_4_temp': None,
-        'circuit_5_temp': None,
-        'circuit_6_temp': None,
-        'circuit_7_temp': None,
-        'circuit_8_temp': None,
-        'co2_content': None,
-        'dhw_pump_0': None,
-        'dhw_pump_1': None,
-        'dhw_pump_2': None,
-        'domestic_hot_water_0_temperature': None,
-        'domestic_hot_water_1_temperature': None,
-        'domestic_hot_water_2_temperature': None,
-        'heating_circulation_program_0': None,
-        'heating_circulation_program_1': None,
-        'heating_circulation_program_2': None,
-        'heating_circulation_program_3': None,
-        'heating_circulation_program_4': None,
-        'heating_circulation_program_5': None,
-        'heating_circulation_program_6': None,
-        'heating_circulation_program_7': None,
-        'heating_circulation_program_8': None,
-        'heating_circulation_pump_0': None,
-        'heating_circulation_pump_1': None,
-        'heating_circulation_pump_2': None,
-        'heating_circulation_pump_3': None,
-        'heating_circulation_pump_4': None,
-        'heating_circulation_pump_5': None,
-        'heating_circulation_pump_6': None,
-        'heating_circulation_pump_7': None,
-        'heating_circulation_pump_8': None,
-        'primary_air': None,
-        'room_3_temperature': None,
-        'room_4_temperature': None,
-        'room_5_temperature': None,
-        'room_6_temperature': None,
-        'room_7_temperature': None,
-        'room_8_temperature': None,
-        'secondary_air': None,
-        'suction_fan': None
     }
 
 @patch("guntamatic.heater.requests.get", side_effect=mock_get)
